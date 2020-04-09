627b9ebe0f6440cdceee834283fd22cd297dbc5d
Leandro Wajswajn <leandrowp@gmail.com>
2020-04-09 17:42:33 -0300

    Improved email format

--- PadletReport/run.ps1
+++ PadletReport/run.ps1
@@ -11,8 +11,6 @@ $Sections = (Invoke-WebRequest -Uri https://padlet.com/wall_sections?wall_id=564
 $Entries = (Invoke-WebRequest -Uri  https://padlet.com/wishes?wall_id=56471570).Content | ConvertFrom-Json
 $LastHour = (Get-Date -Minute 0 -Second 0 -Millisecond 0).AddHours(-7)
 
-$EmailBody = ""
-
 $NewEntries = ForEach($Entry In ($Entries | Where-Object -Property content_updated_at -gt -Value $LastHour)) {
   $Wall = ($Sections | Where-Object -FilterScript {$_.id -eq $Entry.wall_section_id}).Title
   $Title = $Entry.headline
@@ -36,18 +34,31 @@ $NewEntries = ForEach($Entry In ($Entries | Where-Object -Property content_updat
   }
 }
 
+$EmailBody = "<table style=""background-color: #F6F6F6"" width=""100%"" border=""0"" cellspacing=""0"" cellpadding=""0"">"
+$EmailBody += "<tr>"
+$EmailBody += "<td width=""25%"" align=""center"" valign=""top"" style=""font-family:Arial, Helvetica, sans-serif; font-size:2px; color:#ffffff;"">.</td>"
+$EmailBody += "<td width=""50%"" align=""center"" valign=""top"">"
+
 ForEach($Groups In ($NewEntries | Group-Object -Property "Wall")) {
   $EmailBody += "<h1>{0}</h1>" -f $Groups.Name
 
   ForEach($Item In $Groups.Group) {
-    $EmailBody += "<h2><a href=""{1}"">{0}</a></h2>" -f $Item.Title, $Item.Link
+    $EmailBody += "<div style=""background-color: white"">"
+    $EmailBody += "<h3 style=""text-align: center"">{0}</h3>" -f $Item.Title
     $EmailBody += $Item.Body
+    $EmailBody += "<br/><a href=""{0}"" style=""background-color:#72B6CB;border:1px solid #72B6CB;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;"">Leer mas &rarr;</a>" -f $Item.Link
+    $EmailBody += "</div><p>&nbsp;</p>"
   }
 }
 
-if($EmailBody) {
+$EmailBody += "</td>"
+$EmailBody += "<td width=""25%"" align=""center"" valign=""top"" style=""font-family:Arial, Helvetica, sans-serif; font-size:2px; color:#ffffff;"">.</td>"
+$EmailBody += "</tr>"
+$EmailBody += "</table>"
+
+if($NewEntries.Length -gt 0) {
   "Se encontraron novedades, enviandolas por email"
-  $Subject = "JIC N° 9 DE 1 - Padlet Update - {0}" -f $LastHour.AddHours(3).ToString("dd/MM HH:mm")
+  $Subject = "JIC N° 9 DE 1 - Padlet Update - {0}" -f $LastHour.ToString("dd/MM")
   Send-MailMessage -Body $EmailBody -BodyAsHtml -To $Recipient -Subject $Subject -SmtpServer $SmtpServer -Credential $apiCredential -From $From -Encoding utf8
 }
 else {
